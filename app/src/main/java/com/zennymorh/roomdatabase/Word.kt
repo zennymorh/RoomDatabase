e4edeb809fdef58a6d7de65b1998dd313d6efa0a
Zainab Jimoh <jimohzainab4753@gmail.com>
2022-05-19 03:21:18 +0100

    Populated the database

--- app/src/main/java/com/zennymorh/roomdatabase/Word.kt
+++ app/src/main/java/com/zennymorh/roomdatabase/Word.kt
@@ -5,8 +5,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "word_table")
-data class Word(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "word") val word: String
-
-    )
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
--- app/src/main/java/com/zennymorh/roomdatabase/WordApplication.kt
+++ app/src/main/java/com/zennymorh/roomdatabase/WordApplication.kt
@@ -1,12 +1,16 @@
 package com.zennymorh.roomdatabase
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordApplication : Application(){
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
     // We are doing this because we only want one instance of the
     // database and repository
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
